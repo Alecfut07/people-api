550b54ae8cea97bec70ae7e49a86609d56de4856
Alecfut07 <ortega.cisneros.alec@gmail.com>
2020-12-21 21:55:37 -0800

    Aplicar reglas lint a archivos

--- app/categories.js
+++ app/categories.js
@@ -1,154 +1,158 @@
-const ApiBody = require("./models/api-body");
-const ApiError = require("./models/api-error");
-const Category = require("./models/category");
+const ApiBody = require('./models/api-body');
+const ApiError = require('./models/api-error');
+const Category = require('./models/category');
 
 function Categories(connection, app) {
     app.get('/categories', (req, res) => {
         connection.query(
             `
-                SELECT * FROM categories
+                SELECT *
+                FROM categories
             `,
-            function (error, results, fields) {
+            (error, results) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const apiError = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [apiError]);
+                    res.status(500).json(body);
                 } else {
-                    var categories = [];
-                    for (var row of results) {
-                        var category = new Category(row.id, row.name)
-                        categories.push(category)
-                    }
-                    var body = new ApiBody(categories)
-                    res.json(body)
+                    const categories = [];
+                    results.forEach((row) => {
+                        const category = new Category(row.id, row.name);
+                        categories.push(category);
+                    });
+                    const body = new ApiBody(categories);
+                    res.json(body);
                 }
-            }
+            },
         );
-    })
+    });
 
     app.get('/categories/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
+        const id = parseInt(req.params.id, 10);
         connection.query(
             `
-                SELECT * FROM categories WHERE id = ${id}
+                SELECT *
+                FROM categories
+                WHERE id = ${id}
             `,
-            function (error, results, fields) {
+            (error, results) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const apiError = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [apiError]);
+                    res.status(500).json(body);
                 } else if (results.length > 0) {
-                    var row = results[0]
-                    var category = new Category(row.id, row.name)
-                    var body = new ApiBody(category)
-                    res.json(body)
+                    const row = results[0];
+                    const category = new Category(row.id, row.name);
+                    const body = new ApiBody(category);
+                    res.json(body);
                 } else {
-                    var body = new ApiBody([], [])
-                    res.json(body)
+                    const body = new ApiBody([], []);
+                    res.json(body);
                 }
-            }
+            },
         );
-    })
+    });
 
     app.post('/categories', (req, res) => {
         connection.query(
             `
-                INSERT INTO categories(name) VALUES ('${req.body.name}')
+                INSERT INTO categories(name)
+                VALUES ('${req.body.name}')
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const apiError = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [apiError]);
+                    res.status(500).json(body);
                 } else {
-                    var row = req.body
-                    var category = new Category(row.id, row.name)
-                    var body = new ApiBody(category)
-                    res.status(201).json(body)
+                    const row = req.body;
+                    const category = new Category(row.id, row.name);
+                    const body = new ApiBody(category);
+                    res.status(201).json(body);
                 }
-            }
-        )
-    })
+            },
+        );
+    });
 
     app.put('/categories/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
-        var categoryToUpdate = req.body
+        const id = parseInt(req.params.id, 10);
+        const categoryToUpdate = req.body;
         connection.query(
             `
-                UPDATE categories SET name = '${categoryToUpdate.name}' WHERE id = ${id}
+                UPDATE categories
+                SET name = '${categoryToUpdate.name}'
+                WHERE id = ${id}
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const apiError = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [apiError]);
+                    res.status(500).json(body);
                 } else {
                     connection.query(
                         `
-                            SELECT * FROM categories WHERE id = ${id}
+                            SELECT *
+                            FROM categories
+                            WHERE id = ${id}
                         `,
-                        function (error, results, fields) {
+                        (error, results) => {
                             if (error) {
-                                console.log(error)
-                                var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                                var body = new ApiBody([], [error])
-                                res.status(500).json(body)
+                                console.log(error);
+                                const apiError = new ApiError(500, 'INTERNAL SERVER ERROR');
+                                const body = new ApiBody([], [apiError]);
+                                res.status(500).json(body);
                             } else if (results.length > 0) {
-                                var row = results[0]
-                                var category = new Category(row.id, row.name)
-                                var body = new ApiBody(category, [])
-                                res.status(200).json(body)
+                                const row = results[0];
+                                const category = new Category(row.id, row.name);
+                                const body = new ApiBody(category, []);
+                                res.status(200).json(body);
                             } else {
-                                var body = new ApiBody([], [])
-                                res.json(body)
+                                const body = new ApiBody([], []);
+                                res.json(body);
                             }
-                        }
-                    )
+                        },
+                    );
                 }
-            }
-        )
-    })
-
-    app.get('/hello', (req, res) => {
-        res.send('Hello World!');
-    })
+            },
+        );
+    });
 
     app.delete('/categories/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
+        const id = parseInt(req.params.id, 10);
         connection.query(
             `
                 DELETE FROM products WHERE category_id = ${id}
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [error]);
+                    res.status(500).json(body);
                 } else {
                     connection.query(
                         `
                             DELETE FROM categories WHERE id = ${id}
                         `,
-                        function (error, results, fields) {
+                        (error) => {
                             if (error) {
-                                console.log(error)
-                                var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                                var body = new ApiBody([], [error])
-                                res.status(500).json(body)
+                                console.log(error);
+                                const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                                const body = new ApiBody([], [error]);
+                                res.status(500).json(body);
                             }
-                            res.status(204).end()
-                        }
-                    )
+                            res.status(204).end();
+                        },
+                    );
                 }
-            }
+            },
 
-        )
-    })
+        );
+    });
 }
 
-module.exports = Categories
+module.exports = Categories;
--- app/products.js
+++ app/products.js
@@ -1,8 +1,8 @@
-var ApiError = require('./models/api-error');
-var ApiBody = require('./models/api-body');
-var Category = require('./models/category');
-var InfoStock = require('./models/info-stock');
-var Product = require('./models/product');
+const ApiError = require('./models/api-error');
+const ApiBody = require('./models/api-body');
+const Category = require('./models/category');
+const InfoStock = require('./models/info-stock');
+const Product = require('./models/product');
 
 function Products(connection, app) {
     app.get('/products', (req, res) => {
@@ -11,54 +11,55 @@ function Products(connection, app) {
             SELECT p.*, c.id as category_id, c.name as category_name
             FROM products p INNER JOIN categories c ON p.category_id = c.id
             `,
-            function (error, results, fields) {
+            (error, results) => {
                 if (error) {
                     console.log(error);
-                    var error = new ApiError(500, 'INTERNAL SERVER ERROR');
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [error]);
+                    res.status(500).json(body);
                 } else {
-                    var products = [];
-                    for (var row of results) {
-                        var category = new Category(row.category_id, row.category_name);
-                        var infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max);
-                        var product = new Product(row.id, row.name, row.details, category, infoStock, row.price)
+                    const products = [];
+                    results.forEach((row) => {
+                        const category = new Category(row.category_id, row.category_name);
+                        const infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max);
+                        const product = new Product(row.id, row.name, row.details, category, infoStock, row.price);
                         products.push(product);
-                    }
-                    var body = new ApiBody(products)
-                    res.json(body)
+                    });
+                    const body = new ApiBody(products);
+                    res.json(body);
                 }
-                //console.log(results[0]);
-                //console.log(results[0].nombreProducto);
-            });
-    })
+                // console.log(results[0]);
+                // console.log(results[0].nombreProducto);
+            },
+        );
+    });
 
     app.get('/products/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
+        const id = parseInt(req.params.id, 10);
         // connection.query('SELECT * FROM products WHERE idProducto = ' + id)
-        var query = `
+        const query = `
             SELECT p.*, c.id as category_id, c.name as category_name
             FROM products p INNER JOIN categories c ON p.category_id = c.id
             WHERE p.id = ${id}`
-        connection.query(query, function (error, results, fields) {
+        connection.query(query, (error, results) => {
             if (error) {
                 console.log(error);
-                var error = new ApiError(500, "INTERNAL SERVER ERROR");
-                var body = new ApiBody([], [error]);
+                const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                const body = new ApiBody([], [error]);
                 res.status(500).json(body);
             } else if (results.length > 0) {
-                var row = results[0]
-                var infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max)
-                var category = new Category(row.category_id, row.category_name)
-                var product = new Product(row.id, row.name, row.details, category, infoStock, row.price)
-                var body = new ApiBody(product)
-                res.json(body)
+                const row = results[0];
+                const infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max);
+                const category = new Category(row.category_id, row.category_name);
+                const product = new Product(row.id, row.name, row.details, category, infoStock, row.price);
+                const body = new ApiBody(product);
+                res.json(body);
             } else {
-                var body = new ApiBody([], [])
-                res.json(body)
+                const body = new ApiBody([], []);
+                res.json(body);
             }
         });
-    })
+    });
 
     function isProductNameValid(name) {
         return name && name.length > 0;
@@ -69,14 +70,13 @@ function Products(connection, app) {
     }
 
     function isProductValid(product) {
-        return isProductNameValid(product.name) &&
-            isProductDetailsValid(product.details);
+        return isProductNameValid(product.name) && isProductDetailsValid(product.details);
     }
 
     app.post('/products', (req, res) => {
         if (!isProductValid(req.body)) {
-            var error = new ApiError(400, "BAD REQUEST ERROR")
-            var body = new ApiBody([], [error])
+            const error = new ApiError(400, 'BAD REQUEST ERROR');
+            const body = new ApiBody([], [error]);
             res.status(400).json(body);
             return;
         }
@@ -86,27 +86,28 @@ function Products(connection, app) {
             VALUES ('${req.body.name}', '${req.body.details}', ${req.body.stock}, 
             ${req.body.category_id}, ${req.body.price}, ${req.body.stock_max}, ${req.body.stock_min})
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [error]);
+                    res.status(500).json(body);
                 } else {
-                    var row = req.body
-                    var infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max)
-                    var category = new Category(row.category_id, row.category_name)
-                    var product = new Product(row.id, row.name, row.details, category, infoStock, row.price)
-                    var body = new ApiBody(product)
-                    res.status(201).json(body)
+                    const row = req.body;
+                    const infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max);
+                    const category = new Category(row.category_id, row.category_name);
+                    const product = new Product(row.id, row.name, row.details, category, infoStock, row.price);
+                    const body = new ApiBody(product);
+                    res.status(201).json(body);
                 }
-            });
-        //console.log(req.body.nombreProducto)
-    })
+            },
+        );
+        // console.log(req.body.nombreProducto)
+    });
 
     app.put('/products/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
-        var productToUpdate = req.body;
+        const id = parseInt(req.params.id, 10);
+        const productToUpdate = req.body;
 
         console.log(productToUpdate);
         connection.query(
@@ -115,12 +116,12 @@ function Products(connection, app) {
                 SET name = '${productToUpdate.name}', details = '${productToUpdate.details}', category_id = ${productToUpdate.category_id}, price = '${productToUpdate.price}'
                 WHERE id = ${id}
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [error]);
+                    res.status(500).json(body);
                 } else {
                     connection.query(
                         `
@@ -128,22 +129,23 @@ function Products(connection, app) {
                         FROM products p INNER JOIN categories c ON p.category_id = c.id
                         WHERE p.id = ${id}
                         `,
-                        function (error, results, fields) {
+                        (error, results) => {
                             if (error) {
-                                console.log(error)
-                                var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                                var body = new ApiBody([], [error])
-                                res.status(500).json(body)
+                                console.log(error);
+                                const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                                const body = new ApiBody([], [error]);
+                                res.status(500).json(body);
                             } else if (results.length > 0) {
-                                var row = results[0]
-                                var infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max)
-                                var category = new Category(row.category_id, row.category_name)
-                                var product = new Product(row.id, row.name, row.details, category, infoStock, row.price)
-                                var body = new ApiBody(product)
-                                res.status(200).json(body)
+                                const row = results[0];
+                                const infoStock = new InfoStock(row.stock, row.stock_min, row.stock_max);
+                                const category = new Category(row.category_id, row.category_name);
+                                const product = new Product(row.id, row.name, row.details, category, infoStock, 
+                                    row.price);
+                                const body = new ApiBody(product);
+                                res.status(200).json(body);
                             } else {
-                                var body = new ApiBody([], [])
-                                res.json(body)
+                                const body = new ApiBody([], []);
+                                res.json(body);
                             }
                         }
                     )
@@ -153,50 +155,50 @@ function Products(connection, app) {
     })
 
     app.delete('/products/:id', (req, res) => {
-        var id = parseInt(req.params.id, 10)
+        const id = parseInt(req.params.id, 10);
         connection.query(
             `
                 DELETE FROM price_history WHERE product_id = ${id}
             `,
-            function (error, results, fields) {
+            (error) => {
                 if (error) {
-                    console.log(error)
-                    var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                    var body = new ApiBody([], [error])
-                    res.status(500).json(body)
+                    console.log(error);
+                    const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                    const body = new ApiBody([], [error]);
+                    res.status(500).json(body);
                 } else {
                     connection.query(
                         `
                             DELETE FROM orders WHERE product_id = ${id}
                         `,
-                        function (error, results, fields) {
+                        (error) => {
                             if (error) {
-                                console.log(error)
-                                var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                                var body = new ApiBody([], [error])
-                                res.status(500).json(body)
+                                console.log(error);
+                                const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                                const body = new ApiBody([], [error]);
+                                res.status(500).json(body);
                             } else {
                                 connection.query(
                                     `
                                         DELETE FROM products WHERE id = ${id}
                                     `,
-                                    function (error, results, fields) {
+                                    (error) => {
                                         if (error) {
-                                            console.log(error)
-                                            var error = new ApiError(500, "INTERNAL SERVER ERROR")
-                                            var body = new ApiBody([], [error])
-                                            res.status(500).json(body)
+                                            console.log(error);
+                                            const error = new ApiError(500, 'INTERNAL SERVER ERROR');
+                                            const body = new ApiBody([], [error]);
+                                            res.status(500).json(body);
                                         }
-                                        res.status(204).end()
-                                    }
+                                        res.status(204).end();
+                                    },
                                 );
                             }
-                        }
-                    )
+                        },
+                    );
                 }
-            }
-        )
-    })
+            },
+        );
+    });
 }
 
-module.exports = Products
+module.exports = Products;
